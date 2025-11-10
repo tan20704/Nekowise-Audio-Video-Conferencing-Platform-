0bba253a8f85bc1b31708384ef1320460fd73fca
Tanya Verma <167804092+tan20704@users.noreply.github.com>
2025-11-10 21:58:27 +0530

    Delete nekowise.beta directory

--- nekowise.beta/client/src/components/Layout/Container.jsx
+++
@@ -1,3 +0,0 @@
-export default function Container({ children }) {
-  return <div className="container mx-auto px-4 py-8">{children}</div>;
-}
--- nekowise.beta/client/src/features/room/components/index.js
+++
@@ -1,4 +0,0 @@
-// Feature components export
-export { default as VideoGrid } from "./VideoGrid";
-export { default as VideoControls } from "./VideoControls";
-export { default as VideoTile } from "./VideoTile";
